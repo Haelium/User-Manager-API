e17323352336abfa3b29dadd094de157591f9473
haelium <davidbourke94@gmail.com>
2020-01-02 13:23:04 +0000

    Allow flags for specifying redis config

--- cmd/userapi.go
+++ cmd/userapi.go
@@ -3,6 +3,7 @@ package main
 import (
 	//	"encoding/json"
 
+	"flag"
 	"log"
 	"net/http"
 
@@ -12,10 +13,13 @@ import (
 	"github.com/Haelium/BloomReachTest/redisutil"
 )
 
-// TDOD: input args & config struct
-
 func main() {
-	user_db, err := redisutil.NewRedisHashConn("localhost:6379", "", 0)
+	redisAddrPtr := flag.String("redis_address", "localhost", "Redis server address")
+	redisPortPtr := flag.String("redis_port", "6379", "Redis server port")
+	redisPassPtr := flag.String("redis_password", "", "Redis server password")
+	redisDBIndexPtr := flag.Int("redis_db_index", 0, "Redis database index")
+
+	user_db, err := redisutil.NewRedisHashConn((*redisAddrPtr)+":"+(*redisPortPtr), *redisPassPtr, *redisDBIndexPtr)
 	if err != nil {
 		log.Panicf("Exit: %s", err)
 	}
